addccbd73ea60eef0a52e4452ece2e8ac6e03d10
Aashi Tiwari <aashi.tiwari@sjsu.edu>
2017-10-26 00:04:02 -0700

    Create server.js

---
+++ Usersession/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var redis   = require("redis");
+var session = require('express-session');
+var redisStore = require('connect-redis')(session);
+var bodyParser = require('body-parser');
+var client  = redis.createClient();
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
+app.use(session({
+    secret: 'ssshhhhh',
+    // create new redis store.
+    store: new redisStore({ host: 'localhost', port: 6379, client: client,ttl :  260}),
+    saveUninitialized: false,
+    resave: false
+}));

3829722523dfa1830b32799b775b1fd9df8e0c1e
suchishree29 <suchishree.jena29@gmail.com>
2017-10-26 22:56:49 -0700

    updated code from http methods to Express module

--- Product Catalog/app.js
+++ Product Catalog/app.js
@@ -1,15 +1,36 @@
 //Nodejs app to connect to MongoDb
 
-var http = require('http');
+//var http = require('http');
 var express = require('express'),
     cons = require('consolidate'),
     mongodb = require('mongodb');
+	
+	
+	app = express();
 
-var server = http.createServer(function(request, response) {
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.end("Hello, World");
+
+//var server = http.createServer(function(request, response) {
+		//response.writeHead(200, {"Content-Type": "text/plain"});
+		//response.end("Hello, World");
+//});
+
+app.get('/', function(req,res) {
+	res.send('Hello World');
 });
 
-server.listen(8000);
+app.use(function(req,res) {
+	res.sendStatus(404);
+});
 
-console.log("Server running at http://localhost:8000");
+
+var server = app.listen(8000, function(){
+	var port = server.address().port;
+	console.log('Server listening on port %s' , port);
+});
+
+
+
+//server.listen(8000);
+
+//console.log("Server running at http://localhost:8000");
+

7ca1fb7d84675025a22088633caa117e8b65d60c
suchishree29 <suchishree.jena29@gmail.com>
2017-10-27 21:17:00 -0700

    Delete mongoDb.js

--- Product Catalog/mongoDb.js
+++
@@ -1,9 +0,0 @@
-//dummy starter code for nodejs code modularization
-
-var appRouter = function(app) {
- app.get("/mongodb", function(req, res) {
-    res.send("Inside Mongo DB GET method calls");
-});
-}
- 
-module.exports = appRouter;

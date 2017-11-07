1a18a4a7f498c53755cdb6fa83ddaec07cf9e414
Aashi Tiwari <aashi.tiwari@sjsu.edu>
2017-11-07 13:37:36 -0800

    Delete session.py

--- Usersession/session.py
+++
@@ -1,9 +0,0 @@
-class Session
-  attr_accessor :id, :identity_id, :expires_at
-
-  def initialize(attributes)
-    self.id = attributes[:id] || SecureRandom.hex(20)
-    self.identity_id = attributes[:identity_id]
-    self.expires_at = attributes[:expires_at] || 30.days.from_now
-  end
-end

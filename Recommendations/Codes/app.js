8d6ec2969d155e33a9e9dad184f3c65fb45ea8c4
pshriva <shrivastavapranjali@gmail.com>
2017-11-08 23:21:02 -0800

    Update app.js

--- Recommendations/Codes/app.js
+++ Recommendations/Codes/app.js
@@ -1,9 +1,34 @@
 var express = require('express')
-
+var r = require('request')
+var http = require('http')
+var txUrl = 'http://192.168.99.100:7474/db/data/transaction/commit';
+//var txUrl = 'http://192.168.99.100:7474/db/data/cypher';
 var app = express()
 app.use(express.logger('dev'))
+app.set('port', 3000)
 app.get('/', function(req, res){
-	res.end('Hi there!');
+	res.writeHead(200, {'content-type' : 'text/plain'})
+	sayHello(req, res)
+	
 })
-console.log( "Server running on Port 3000..." ) ;
-app.listen(3000)
+function sayHello(req, res){
+	res.end('Hi there')
+}
+app.get('/importpd', function(req, res){
+	var query='MATCH (n) RETURN COUNT(n)'
+	var params={limit: 10}
+	var cb=function(err,data) { res.end(JSON.stringify(data)) }
+	cypher(query, params, cb, res)
+
+})
+function cypher(query,params,cb, res) {
+  r.post({uri:txUrl,
+          json:{statements:[{statement:query,parameters:params}]}},
+         function(err,res) { cb(err,res.body)})
+  res.writeHead(200, {'content-type' : 'application/json'})
+  //res.end(body)
+  //res.end(cb(err,res.body))
+}
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'))
+})

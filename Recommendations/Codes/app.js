7a544925177760abbbf50b2fbd1b64a33349d9b2
pshriva <shrivastavapranjali@gmail.com>
2017-11-09 01:35:10 -0800

    Update app.js

--- Recommendations/Codes/app.js
+++ Recommendations/Codes/app.js
@@ -1,34 +1,43 @@
 var express = require('express')
-var r = require('request')
-var http = require('http')
-var txUrl = 'http://192.168.99.100:7474/db/data/transaction/commit';
+//, routes = require('./routes')
+, neo4j = require('./routes/neo4j')
+, http = require('http')
+, txUrl = 'http://192.168.99.100:7474/db/data/transaction/commit'
 //var txUrl = 'http://192.168.99.100:7474/db/data/cypher';
 var app = express()
 app.use(express.logger('dev'))
 app.set('port', 3000)
 app.get('/', function(req, res){
-	res.writeHead(200, {'content-type' : 'text/plain'})
+	res.writeHead(200, {'content-type' : 'application/json'})
 	sayHello(req, res)
 	
 })
 function sayHello(req, res){
-	res.end('Hi there')
+	res.end('{"Salutation" : "Hi there"}')
 }
-app.get('/importpd', function(req, res){
-	var query='MATCH (n) RETURN COUNT(n)'
-	var params={limit: 10}
-	var cb=function(err,data) { res.end(JSON.stringify(data)) }
-	cypher(query, params, cb, res)
-
-})
-function cypher(query,params,cb, res) {
-  r.post({uri:txUrl,
-          json:{statements:[{statement:query,parameters:params}]}},
-         function(err,res) { cb(err,res.body)})
-  res.writeHead(200, {'content-type' : 'application/json'})
-  //res.end(body)
-  //res.end(cb(err,res.body))
-}
+app.get('/storeData', neo4j.storeData)
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'))
 })
+
+// /db/data/cypher
+
+// {
+// 	"title" : "adidas Originals Women's Superstar W Fashion Sneaker",
+// 	"price" : "$179.99",
+// 	"stock" : "13",
+// 	"description" : "Leather/Synthetic Rubber sole Retro-inspired sneaker featuring classic rubber shell toe Breathable mesh liner",
+// 	"imageBin" : "This is just sample because I dont know what is imageBin",
+// 	"imageUrl" : "https://slimages.macysassets.com/is/image/MCY/products/4/optimized/3982934_fpx.tif??op_sharpen=1&fit=fit,1&$filterlrg$&wid=1200&hei=1467",
+// 	"categories" : "Shoes"
+// }
+
+// {
+// 	"title" : "Steve Madden",
+// 	"price" : "$118.00",
+// 	"stock" : "12",
+// 	"description" : "Steve Madden Women's Ecentrcq Slip-On Fashion Sneaker",
+// 	"imageBin" : "This is just sample because I dont know what is imageBin",
+// 	"imageUrl" : "https://images-na.ssl-images-amazon.com/images/I/81YSJCG3WqL._UL1500_.jpg",
+// 	"categories" : "Shoes"
+// }

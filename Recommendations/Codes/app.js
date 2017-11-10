0ee7ff91e0735827bdee0eac9190c3626192ea40
pshriva <shrivastavapranjali@gmail.com>
2017-11-10 00:47:12 -0800

    Update app.js

--- Recommendations/Codes/app.js
+++ Recommendations/Codes/app.js
@@ -1,12 +1,15 @@
 var express = require('express')
 //, routes = require('./routes')
 , neo4j = require('./routes/neo4j')
+, bodyParser = require('body-parser')
 , http = require('http')
 , txUrl = 'http://192.168.99.100:7474/db/data/transaction/commit'
 //var txUrl = 'http://192.168.99.100:7474/db/data/cypher';
 var app = express()
-app.use(express.logger('dev'))
 app.set('port', 3000)
+app.use(express.logger('dev'))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.get('/', function(req, res){
 	res.writeHead(200, {'content-type' : 'application/json'})
 	sayHello(req, res)
@@ -15,7 +18,13 @@ app.get('/', function(req, res){
 function sayHello(req, res){
 	res.end('{"Salutation" : "Hi there"}')
 }
-app.get('/storeData', neo4j.storeData)
+app.post('/storeData', neo4j.storeData)
+app.get('/suggestSimilarProducts', neo4j.suggestSimilarProducts)
+app.get('/getProductCatalog', function(req, res){
+	var data = '[{"title" : "Steve Madden","price" : "$118.00","stock" : "12", "description" : "Steve Madden Women Ecentrcq Slip-On Fashion Sneaker","categories" : "Shoes"},{"title" : "Steve Madden","price" : "$118.00","stock" : "12", "description" : "Steve Madden Women Ecentrcq Slip-On Fashion Sneaker","categories" : "Shoes"}]';
+	res.writeHead(200, {'content-type' : 'application/json'})
+	res.end(data)
+})
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'))
 })
@@ -41,3 +50,5 @@ http.createServer(app).listen(app.get('port'), function(){
 // 	"imageUrl" : "https://images-na.ssl-images-amazon.com/images/I/81YSJCG3WqL._UL1500_.jpg",
 // 	"categories" : "Shoes"
 // }
+
+

5ca251c43ee9bda98da1f5964aeb3705fc1d04e0
SadabQ <shadab.qureshi88@gmail.com>
2017-11-09 21:10:25 -0800

    Update User.java

--- user-activity-log/CassandraDemo/src/main/java/user-activity-log/entity/User.java
+++ user-activity-log/CassandraDemo/src/main/java/user-activity-log/entity/User.java
@@ -1,4 +1,4 @@
-package user-activity-log.entity;
+package useractivitylog.entity;
 
 import java.util.Date;
 import java.util.UUID;
@@ -11,8 +11,8 @@ import com.datastax.driver.mapping.annotations.Column;
 import com.datastax.driver.mapping.annotations.PartitionKey;
 import com.datastax.driver.mapping.annotations.Table;
 
-import killrvideo.user_management.UserManagementServiceOuterClass.UserProfile;
-import killrvideo.utils.TypeConverter;
+import useractivitylog.user_management.UserManagementServiceOuterClass.UserProfile;
+import useractivitylog.utils.TypeConverter;
 
 @Table(keyspace = Schema.KEYSPACE, name = "users")
 public class User {
@@ -46,5 +46,55 @@ public class User {
         this.email = email;
         this.createdAt = createdAt;
     }
+    
+    public UUID getUserid() {
+        return userid;
+    }
+
+    public void setUserid(UUID userid) {
+        this.userid = userid;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public UserProfile toUserProfile() {
+        return UserProfile.newBuilder()
+                .setEmail(email)
+                .setFirstName(firstname)
+                .setLastName(lastname)
+                .setUserId(TypeConverter.uuidToUuid(userid))
+                .build();
+    }
+}
     
     

422cad7d5ab3a9af7e35ac1f796102ab3afa63c8
pshriva <shrivastavapranjali@gmail.com>
2017-11-11 03:39:30 -0800

    Update neo4j.js

--- Recommendations/Codes/routes/neo4j.js
+++ Recommendations/Codes/routes/neo4j.js
@@ -1,18 +1,37 @@
-var http = require('http')
-, r = require('request')
-, txUrl = 'http://192.168.99.100:7474/db/data/transaction/commit'
+//var http = require('http')
+var r = require('request')
+, dbUrl = 'http://192.168.99.100:7474/db/data/transaction/commit'
 
+
+//this function is used to create a graph database from the available products fetched from ProductCatalog
 exports.storeData = function(req, res){
-	var query='MATCH (n) RETURN COUNT(n)'
+	console.log(req.body)
+	var query='CREATE (n)'
 	var params={limit: 10}
-	var cb=function(err,data) { res.end(JSON.stringify(data)) }
+	//var cb=function(err,data) { res.end(JSON.stringify(data)) }
+	var cb=function(err,data) { res.end(JSON.stringify('success')) }
 	cypher(query, params, cb, res)
 }
+
+// Helper function for storData
 function cypher(query,params,cb, res) {
-  r.post({uri:txUrl,
+  r.post({uri:dbUrl,
           json:{statements:[{statement:query,parameters:params}]}},
          function(err,res) { cb(err,res.body)})
   res.writeHead(200, {'content-type' : 'application/json'})
-  //res.end(body)
-  //res.end(cb(err,res.body))
+}
+
+//
+exports.suggestSimilarProducts = function(req, res){
+	
+}
+exports.importProductCatalog = function(req, res){
+	var callback = function(err, data){res.end(data)}
+	var pcUrl = 'http://10.0.0.90:3000/getProductCatalog'
+	getProductCatalogData(pcUrl, res, callback)
+}
+
+function getProductCatalogData(pcUrl, res, callback){
+	r.get(pcUrl, function(err, res){callback(err, res.body)})
+	res.writeHead(200, {'content-type' : 'application/json'})
 }

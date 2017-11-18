9139acedb9df8a1250e0f1de3cf72b33061386d4
Utsav Jain <utsavjain1408@gmail.com>
2017-11-17 22:31:14 -0800

    Adding Hackathon code

--- Shopping Cart/Hackathon/server.js
+++ Shopping Cart/Hackathon/server.js
@@ -2,18 +2,35 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import {urlSchema} from './urlSchema';
-
-const UrlModel = mongoose.model('urlSchma', urlSchema);
+var amqp = require('amqplib/callback_api');
+const UrlModel = mongoose.model('urlSchema', urlSchema);
+//bitnami@urlshorterMongodb:/opt/bitnami/mongodb$ mongo admin --username root -p^C
+//mongo 40.121.205.206:27017/urldb  --username utsav -p
+/*amqp.connect('amqp://localhost', function(err, conn) {
+	conn.createChannel(function(err, ch) {
+	  var q = 'hello';
+  
+	  ch.assertQueue(q, {durable: false});
+	  // Note: on Node 6 Buffer.from(msg) should be used
+	  ch.sendToQueue(q, new Buffer('Hello World!'));
+	  console.log(" [x] Sent 'Hello World!'");
+	  console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+	  ch.consume(q, function(msg) {
+		console.log(" [x] Received %s", msg.content.toString());
+	  }, {noAck: true});
+	});
+  });*/
 var app = express();
 
-mongoose.Promise = global.Promise;
-var UrlMongoConnection = mongoose.connect('mongodb://localhost:27017/urldb', {
-	useMongoClient: true
-  });
-/*var UrlMongoConnection = mongoose.connect('mongodb://utsavjain1408:Password1.@40.121.205.2056:27017/urldb', {
-  useMongoClient: true
-});*/
 
+/*var UrlMongoConnection = mongoose.connect('mongodb://localhost:27017/urldb', {
+	useMongoClient: true
+  });*/
+var mongourl =  'mongodb://utsav:1234@40.121.205.206:27017/urldb'; 
+var UrlMongoConnection = mongoose.connect(mongourl, {
+	useMongoClient: true
+});
+mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
@@ -23,6 +40,18 @@ app.route('/').get((req, res) => {
 
 app.route('/url').post((req,res) => {
 	console.log('Url post ho raha hai');
+	/*amqp.connect('amqp://localhost', function(err, conn) {
+		conn.createChannel(function(err, ch) {
+		  var q = 'hello';
+		  var msg = 'Hello World!';
+	  
+		  ch.assertQueue(q, {durable: false});
+		  // Note: on Node 6 Buffer.from(msg) should be used
+		  ch.sendToQueue(q, new Buffer.from(msg));
+		  console.log(" [x] Sent %s", msg);
+		});
+		setTimeout(function() { conn.close()}, 500);
+	  });*/
 	let newUrl = new UrlModel(req.body);
 	newUrl.save((err, someUrl) => {
 		if(err){
@@ -44,6 +73,17 @@ app.route('/url').post((req,res) => {
 })
 app.route('/url/:shortUrl').get((req, res) => {
 	console.log(`URL GET call hua hai`);
+	/*amqp.connect('amqp://localhost', function(err, conn) {
+	  conn.createChannel(function(err, ch) {
+		var q = 'hello';
+	
+		ch.assertQueue(q, {durable: false});
+		console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+		ch.consume(q, function(msg) {
+		  console.log(" [x] Received %s", msg.content.toString());
+		}, {noAck: true});
+	  });
+	});*/
 	UrlModel.find({shortlink:req.params.shortUrl}, (err, somelongUrl) => {
 		if(err){
 			res.send(`Mongo GET me fat raha hai ${err}`);

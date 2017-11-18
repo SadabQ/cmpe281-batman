fbce147d534c5191e0e53ddb1b693c469099fd19
Aashi Tiwari <aashi.tiwari@sjsu.edu>
2017-11-17 20:14:29 -0800

    Create app.js

---
+++ Usersession/final/app.js
@@ -0,0 +1,6 @@
+var session = require('./demo');
+
+
+var session_create_data = { login_id: "aashi", cart_id : "cart1", quantity: 1};
+var session_set_data = { token: null, login_id: "aashi", cart_id : "cart2", quantity: 2};
+var session_data = {token: null};
